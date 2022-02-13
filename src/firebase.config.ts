4e9d28bb14954f2a3cb0888a775edf414aa69447
Sebastian <sebastian.zawolanski@gmail.com>
2022-02-13 20:47:46 +0100

    feat: setup firebase

---
+++ src/firebase.config.ts
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { initializeApp } from 'firebase/app';
+import { ENV } from './config/env';
+
+const firebaseConfig = {
+  apiKey: ENV.FIREBASE_API_KEY,
+  authDomain: ENV.FIREBASE_AUTH_DOMAIN,
+  projectId: ENV.FIREBASE_PROJEKT_ID,
+  storageBucket: ENV.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: ENV.FIREBASE_MESSAGING_SENDER_ID,
+  appId: ENV.FIREBASE_APP_ID,
+};
+
+initializeApp(firebaseConfig);
