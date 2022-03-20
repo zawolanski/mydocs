ec575669dbf45ffe4b5e4f0210d5fc338e8f0910
Sebastian <sebastian.zawolanski@gmail.com>
2022-03-20 16:27:16 +0100

    feat: add mui components to sign in form

--- src/pages/Root/index.tsx
+++ src/pages/Root/index.tsx
@@ -6,19 +6,23 @@ import SignUp from '@pages/SignUp';
 import Documents from '@pages/Documents';
 import ForgotPassword from '@pages/ForgotPassword';
 import AuthRoute from '@components/Routes/authRoute';
+import { theme } from '@/theme/mui';
+import { ThemeProvider } from '@mui/material/styles';
 
 const Root = () => (
-  <Router basename="mydocs">
-    <Routes>
-      <Route index element={<App />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/documents" element={<AuthRoute />}>
-        <Route path="/documents" element={<Documents />} />
-      </Route>
-    </Routes>
-  </Router>
+  <ThemeProvider theme={theme}>
+    <Router basename="mydocs">
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/documents" element={<AuthRoute />}>
+          <Route path="/documents" element={<Documents />} />
+        </Route>
+      </Routes>
+    </Router>
+  </ThemeProvider>
 );
 
 export default Root;
