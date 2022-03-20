7497bd798e968f4a18a72548453be6c62def595b
Sebastian <sebastian.zawolanski@gmail.com>
2022-03-20 16:46:21 +0100

    feat: add the show password button to sign in form

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,13 +1,24 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-import { Box, Button, Divider, LinearProgress, TextField, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  Checkbox,
+  Divider,
+  FormControlLabel,
+  LinearProgress,
+  TextField,
+  Typography,
+  Link as MuiLink,
+} from '@mui/material';
 import { ReactComponent as Logo } from '@/assets/logo/google.svg';
 
 const SignIn = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
 
   const onSubmit = async (e: React.SyntheticEvent) => {
@@ -33,70 +44,85 @@ const SignIn = () => {
 
   return (
     <Box sx={{ minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <Box
-        sx={{
-          overflow: 'hidden',
-          position: 'relative',
-          border: (theme) => ({ sm: `1px solid ${theme.palette.grey['300']}` }),
-          maxWidth: { xs: '100%', sm: '400px' },
-          width: '100%',
-          p: 5,
-          borderRadius: (theme) =>
-            typeof theme.shape.borderRadius === 'string'
-              ? parseInt(theme.shape.borderRadius, 10) / 2
-              : theme.shape.borderRadius / 2,
-        }}
-      >
-        {isLoading ? <LinearProgress sx={{ width: '100%', position: 'absolute', top: 0, left: 0 }} /> : null}
-        {isLoading ? (
-          <Box sx={{ top: 0, left: 0, bottom: 0, right: 0, opacity: 0.5, position: 'absolute', zIndex: 2 }} />
-        ) : null}
+      <Box sx={{ maxWidth: { xs: '100%', sm: '450px' }, width: '100%' }}>
         <Box
-          component="form"
-          onSubmit={onSubmit}
           sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            opacity: isLoading ? 0.5 : 1,
+            boxSizing: 'border-box',
+            overflow: 'hidden',
+            position: 'relative',
+            border: (theme) => ({ sm: `1px solid ${theme.palette.grey['300']}` }),
+            maxWidth: { xs: '100%', sm: '450px' },
+            width: '100%',
+            p: 5,
+            borderRadius: (theme) =>
+              typeof theme.shape.borderRadius === 'string'
+                ? parseInt(theme.shape.borderRadius, 10) / 2
+                : theme.shape.borderRadius / 2,
           }}
         >
-          <Typography variant="h6" sx={{ mb: 1.25 }}>
-            Sign in
-          </Typography>
-          <Typography variant="body2" sx={{ mb: 2.25 }}>
-            Use your email and password
-          </Typography>
-          <Box sx={{ display: 'flex', flexDirection: 'column', gap: 3, width: '80%', mb: 1.5 }}>
-            <TextField label="Email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-            <Box sx={{ width: '100%' }}>
-              <TextField
-                sx={{ width: '100%' }}
-                label="Password"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <Button component={Link} to="/forgot-password" size="small" sx={{ mt: 0.75, pl: 0, pr: 0 }}>
+          {isLoading ? <LinearProgress sx={{ width: '100%', position: 'absolute', top: 0, left: 0 }} /> : null}
+          {isLoading ? (
+            <Box sx={{ top: 0, left: 0, bottom: 0, right: 0, opacity: 0.5, position: 'absolute', zIndex: 2 }} />
+          ) : null}
+          <Box
+            component="form"
+            onSubmit={onSubmit}
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              opacity: isLoading ? 0.5 : 1,
+            }}
+          >
+            <Typography variant="h6" sx={{ mb: 1.25 }}>
+              Sign in
+            </Typography>
+            <Typography variant="body2" sx={{ mb: 2.25 }}>
+              Use your email and password
+            </Typography>
+            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 3, width: '90%', mb: 1.5 }}>
+              <TextField label="Email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+              <Box sx={{ width: '100%' }}>
+                <TextField
+                  sx={{ width: '100%' }}
+                  label="Password"
+                  type={showPassword ? 'text' : 'password'}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <FormControlLabel
+                  control={<Checkbox onChange={() => setShowPassword((prevState) => !prevState)} />}
+                  label="Show password"
+                />
+              </Box>
+            </Box>
+            <Divider sx={{ mb: 2 }}>or</Divider>
+            <Button variant="outlined" size="large" sx={{ width: '90%', mb: 4, p: '14px 14px' }}>
+              <Box sx={{ height: 25, width: 25, mr: 1 }}>
+                <Logo />
+              </Box>
+              Sign in with Google
+            </Button>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '90%' }}>
+              <Button component={Link} to="/forgot-password" sx={{ mt: 0.75, pl: 0, pr: 0 }}>
                 Forgot password?
+              </Button>
+              <Button type="submit" variant="contained" disabled={isLoading}>
+                Sign in
               </Button>
             </Box>
           </Box>
-          <Divider sx={{ mb: 2 }}>or</Divider>
-          <Button variant="outlined" size="large" sx={{ width: '80%', mb: 4, p: '16.5px 14px' }}>
-            <Box sx={{ height: 25, width: 25, mr: 1 }}>
-              <Logo />
-            </Box>
-            Sign in with Google
-          </Button>
-          <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '80%' }}>
-            <Button component={Link} to="/signup" sx={{ pl: 0, pr: 0 }}>
-              Create account?
-            </Button>
-            <Button type="submit" variant="contained" disabled={isLoading}>
-              Sign in
-            </Button>
-          </Box>
+        </Box>
+        <Box sx={{ width: '100%', display: 'flex', justifyContent: 'flex-end' }}>
+          <MuiLink
+            variant="body2"
+            underline="none"
+            component={Link}
+            to="/signup"
+            sx={{ mt: 1.75, color: 'text.primary' }}
+          >
+            Create account?
+          </MuiLink>
         </Box>
       </Box>
     </Box>
